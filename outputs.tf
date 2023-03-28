c27e4acac7a52159ffe1e2c682ab200e855313e0
Liza Shakury <42377481+LizaShak@users.noreply.github.com>
2023-03-28 19:05:34 +0300

    Output endpoints and suffixes separately (#5)
    * Output endpoints and suffixes separately
    
    * CR changes
    
    * Rename outputs

--- outputs.tf
+++ outputs.tf
@@ -2,10 +2,26 @@ output "private_links" {
   value = local.private_links[var.arm_environment]
 }
 
-output "suffixes" {
-  value = local.suffixes[var.arm_environment]
+output "storage_suffix" {
+  value = local.suffixes[var.arm_environment].storage_endpoint_suffix
 }
 
-output "endpoints" {
-  value = local.endpoints[var.arm_environment]
+output "acr_suffix" {
+  value = local.suffixes[var.arm_environment].acr_server_suffix
+}
+
+output "web_app_suffix" {
+  value = local.suffixes[var.arm_environment].web_app_suffix
+}
+
+output "active_directory_endpoint" {
+  value = local.endpoints[var.arm_environment].active_directory_endpoint
+}
+
+output "microsoft_graph_endpoint" {
+  value = local.endpoints[var.arm_environment].microsoft_graph_endpoint
+}
+
+output "resource_manager_endpoint" {
+  value = local.endpoints[var.arm_environment].resource_manager_endpoint
 }
